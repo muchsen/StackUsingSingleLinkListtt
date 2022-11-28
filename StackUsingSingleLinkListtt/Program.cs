b96f4653f1ed84963dae13d647e15bb8e30a08aa
TA1059 <muchsen.ft21@mail.umy.ac.id>
2022-11-28 16:24:19 +0700

    Add project files.

---
+++ StackUsingSingleLinkListtt/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackUsingSingleLinkList
+{
+    class node
+    {
+        public int info;
+        public node text;
+        public node(int i, node n)
+        {
+            info = i;
+            nrxt = n;
+        }
+    }
+}
